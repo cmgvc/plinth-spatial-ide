3c45f950b554c3724bc050fbe7c024fa8981c00d
cmgvc <chloe.gavv@gmail.com>
2026-03-17 13:04:27 -0400

    create main

---
+++ apps/client/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Canvas from './components/Canvas'; 
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Canvas />
+  </React.StrictMode>
+);

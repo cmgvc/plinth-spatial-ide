475c2b1b07dd981c72946d486071e657aadcf3ce
cmgvc <chloe.gavv@gmail.com>
2026-03-17 12:52:59 -0400

    update workflow test

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
   testMatch: "**/*.spec.ts",
   testIgnore: ["**/node_modules/**", "**/src/**", "**/*.test.ts"],
-
   fullyParallel: true,
+  
   use: {
-    baseURL: "http://127.0.0.1:5173/", 
+    baseURL: "http://127.0.0.1:5173/",
   },
+
   webServer: {
     command: "npm run dev",
-    url: "http://127.0.0.1:5173/", 
+    url: "http://127.0.0.1:5173/",
     reuseExistingServer: !process.env.CI,
-    stdout: 'ignore',
+    timeout: 180 * 1000,
+    stdout: 'pipe',
     stderr: 'pipe',
   },
 });

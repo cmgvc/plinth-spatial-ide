91352eab486fe6ed823f0ae129d96fbb8e2b884c
cmgvc <chloe.gavv@gmail.com>
2026-03-17 11:59:30 -0400

    separate cli steps in workflow

--- apps/client/vitest.config.ts
+++ apps/client/vitest.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    // Only look for files in the utils/src directory
     include: ['src/utils/**/*.{test,spec}.ts'],
+    // Specifically tell it to stay out of the e2e folder
     exclude: ['**/e2e/**', '**/node_modules/**'],
   },
 });

085c03d5ff6b50e9115ea3371f0ad97711c7ca2b
cmgvc <chloe.gavv@gmail.com>
2026-03-17 12:49:13 -0400

    update url path test

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -2,18 +2,18 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-
   testMatch: "**/*.spec.ts",
-
   testIgnore: ["**/node_modules/**", "**/src/**", "**/*.test.ts"],
 
   fullyParallel: true,
   use: {
-    baseURL: "http://localhost:5173/",
+    baseURL: "http://127.0.0.1:5173/", 
   },
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5173/",
+    url: "http://127.0.0.1:5173/", 
     reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
   },
 });

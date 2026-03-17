0f16a87467d87e1c3b3dc5f93c42275219f0d6b8
cmgvc <chloe.gavv@gmail.com>
2026-03-17 12:12:33 -0400

    add playwright ignore folders

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './e2e', 
-  
-  testMatch: '**/*.spec.ts', 
-  
-  testIgnore: '**/src/**', 
+  testDir: "./e2e",
+
+  testMatch: "**/*.spec.ts",
+
+  testIgnore: ["**/node_modules/**", "**/src/**", "**/*.test.ts"],
 
   fullyParallel: true,
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: "http://localhost:5173",
   },
   webServer: {
-    command: 'npm run dev -w apps/client',
-    url: 'http://localhost:5173',
+    command: "npm run dev -w apps/client",
+    url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
 });

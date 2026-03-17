e3a81fa65312952352c32a758a0fd6c60473ac75
cmgvc <chloe.gavv@gmail.com>
2026-03-17 12:05:01 -0400

    add playwright config

--- apps/client/vitest.config.ts
+++ apps/client/vitest.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    // Only look for files in the utils/src directory
     include: ['src/utils/**/*.{test,spec}.ts'],
-    // Specifically tell it to stay out of the e2e folder
     exclude: ['**/e2e/**', '**/node_modules/**'],
   },
 });
